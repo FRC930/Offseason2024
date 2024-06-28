93975fa558a818c4393444f2af4d1070e5fae7b6
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-06-27 20:24:32 -0500

    Evan Callies 6/27/24 Made intake subsystem made shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,52 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.IOs.TalonRollerIO;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    private TalonRollerIO m_intakeMotor;
+
+     /**
+    * <h3>setIntakeSpeed</h3>
+    * @param speed the speed the motor will be set to
+    */
+    public void setSpeed(double speed) {
+        m_intakeMotor.setSpeed(speed);
+    }
+
+    /**
+    * <h3>stop</h3>
+    * This sets the intake's speed to 0
+    */
+    public void stop() {
+        setSpeed(0);
+    }
+
+    /**
+    * <h3>getSpeed</h3>
+    * @return the speed of the intake
+    */
+    public double getSpeed() {
+        return m_intakeMotor.getSpeed();
+    }
+
+    /**
+     * <h3>getVoltage</h3>
+     * @return current applied voltage to Talon
+     */
+    public double getVoltage() {
+        return m_intakeMotor.getVoltage();
+    }
+
+    public void periodic() {
+        m_intakeMotor.runSim();
+        
+    }
+
+    public InstantCommand newSetSpeedCommand(double speed) {
+        return new InstantCommand(() -> setSpeed(speed), this);
+    }
+}
